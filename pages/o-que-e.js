9c0fa70a1eaf5e92653caa840debd4584b30528a
Diogo Vianna V. Araújo <diogovaraujo@gmail.com>
2018-05-08 20:35:34 -0300

    Updating o que e

--- pages/o-que-e.js
+++ pages/o-que-e.js
@@ -25,7 +25,8 @@ export default class Oquee extends React.Component {
           </ContentWrapper>
         </div>
         <ContentWrapper style={pageStyle}>
-          <p className='summary'>O Fora é sobre outras maneiras de viver na cidade e acontece por meio de pesquisas sociais, ações de cultura e transformação da paisagem urbana.</p>
+          <P><div className='title'>/Sobre</div></P>
+          <P>Para pensar histórias, urgências e possíveis futuros dos grandes centros urbanos, toma como ponto de partida a escuta da população — na internet e nas ruas — e a análise de dados para levantar percepções, desejos e demandas ligadas ao espaço público. Em paralelo, com um canal de comunicação cujo objetivo é estimular a imaginação sobre a cidade e engajar nos processos de pesquisa, aborda questões de interesse global a partir de perspectivas locais, abrindo espaço para uma diversidade de manifestações culturais que refletem sobre a vida comum e estimulam práticas transformadoras.</P>
           <Img src='http://zonewallpaper.net/wp-content/uploads/2017/10/Best-4K-Wallpaper-2017.jpg' />
           <P>Para pensar histórias, urgências e possíveis futuros dos grandes centros urbanos, toma como ponto de partida a escuta da população — na internet e nas ruas — e a análise de dados para levantar percepções, desejos e demandas ligadas ao espaço público. Em paralelo, com um canal de comunicação cujo objetivo é estimular a imaginação sobre a cidade e engajar nos processos de pesquisa, aborda questões de interesse global a partir de perspectivas locais, abrindo espaço para uma diversidade de manifestações culturais que refletem sobre a vida comum e estimulam práticas transformadoras.</P>
           <P>Para pensar histórias, urgências e possíveis futuros dos grandes centros urbanos, toma como ponto de partida a escuta da população — na internet e nas ruas — e a análise de dados para levantar percepções, desejos e demandas ligadas ao espaço público. Em paralelo, com um canal de comunicação cujo objetivo é estimular a imaginação sobre a cidade e engajar nos processos de pesquisa, aborda questões de interesse global a partir de perspectivas locais, abrindo espaço para uma diversidade de manifestações culturais que refletem sobre a vida comum e estimulam práticas transformadoras.</P>
@@ -48,6 +49,9 @@ export default class Oquee extends React.Component {
             font-size: 29px;
             top: 49%;
           }
+          .title {
+            margin-left: 30px;
+          }
         `}</style>
       </PageWrapper>
     )
@@ -63,5 +67,5 @@ const summaryStyle = {
   marginLeft: 100,
   marginTop: 48,
   marginBottom: 48,
-  fontSize: 20
+  fontSize: 24
 }
