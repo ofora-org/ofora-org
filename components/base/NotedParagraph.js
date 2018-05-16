d4e031171d5240213a0cec46d42a1300ae657af1
Diogo Vianna V. Araújo <diogovaraujo@gmail.com>
2018-05-15 23:07:10 -0300

    updated paragraph note position on mobile

--- components/base/NotedParagraph.js
+++ components/base/NotedParagraph.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import P from '~/components/base/Paragraph'
+import { MobileOnly, DesktopOnly } from '~/components/struct/Media'
 
 const NotedParagraph = ({ text, note, style }) => (
   <div style={{ ...baseStyle, ...style }}>
     <P>{ text }</P>
-    <P style={noteStyle}>{ note }</P>
+    <div className='note'><P style={noteStyle}>{ note }</P></div>
+    <style jsx>{`
+      @media only screen and (min-width: 752px) {
+        .note {
+          position: absolute;
+          right: 0;
+          top: 0;
+          width: 160px;
+        }
+      }
+    `}</style>
   </div>
 )
 
@@ -15,12 +26,8 @@ const baseStyle = {
 
 const noteStyle = {
   fontSize: 12,
-  position: 'absolute',
-  right: 0,
-  top: 0,
   padding: 0,
   marginTop: '-1em',
-  width: 160
 }
 
 export default NotedParagraph
